8ee31720bc4e7ad3a0f9ec861b3454b0e97e4219
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-05-03 11:45:58 +0100

    make it green

--- test/game/view/game_page_test.dart
+++ test/game/view/game_page_test.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:watchsteroids/game/game.dart';
 
+import '../../helpers/helpers.dart';
+
 void main() {
+  initHydratedBloc();
+
   group('GamePage', () {
     group('route', () {
       test('is routable', () {
